3db086f82443616805a980f29eb52cad23bec071
zadieblack <37587898+zadieblack@users.noreply.github.com>
2018-04-12 14:05:38 -0400

    argh

--- run_as_task.py
+++ run_as_task.py
@@ -3,7 +3,7 @@
 
 import logging
 import socket
-import sys
+import sys, argparse, datetime, threading, traceback
 
 lock_socket = None  # we want to keep the socket open until the very end of
                     # our script so we use a global variable to avoid going
@@ -28,7 +28,20 @@ if not is_lock_free():
 
 # then, either include the rest of your script below,
 # or import it, if it's in a separate file:
+import bodyparts
+import locations
+import names
+import people
+import verbs
+import misc
+import scenes
+
+from io import BytesIO
+from random import *
 from util import *
+from generators import *
+from twitter_stuff import *
+
 from lust_bot import *
 
 InitBot(2400, 300, bTweet = True, bLoop = True)
